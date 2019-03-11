467d0dd259e28e70a1747aaeee0232d5b4ccc3d4
Justine B <justinegayton@gmail.com>
2019-03-11 20:30:06 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0 
-function init(e) {
+
+function init() {
   const key = parseInt(e.detail || e.which);
   console.log(`${e.detail}`);
   console.log(`${e.detail}`);
