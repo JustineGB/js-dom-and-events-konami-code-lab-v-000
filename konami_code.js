83dadeba90ecee0b69d3e9ea888f47aa8c016813
Justine B <justinegayton@gmail.com>
2019-03-11 20:23:19 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   const key = parseInt(e.detail || e.which);
  
-  if (key === alphabet[index]) {
+  if (key === code[index]) {
     index++;
  
     if (index === alphabet.length) {
