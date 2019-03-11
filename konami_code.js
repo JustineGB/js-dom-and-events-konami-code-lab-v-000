eccf6549753832a2335fa664074582a936a33bfe
Justine B <justinegayton@gmail.com>
2019-03-11 20:36:06 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -2,27 +2,6 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 let index = 0 
 
-
-  const key = parseInt(e.location || e.which);
-  console.log(`${e.detail}`);
-  console.log(`${e.which}`);
-  
-  if (key === code[index]) {
-    index++;
- 
-    if (index === code.length) {
-      alert("Hurray!");
- 
-      index = 0;
-    }
-  } else {
-    index = 0;
-  }
-}
-
-
-
-
 function init() {
   document.body.addEventListener('keydown', function(e){
   const key = parseInt(e.detail || e.location);
@@ -39,4 +18,6 @@ function init() {
       index = 0;
     }
   });
+  console.log(`${e.detail}`);
+  console.log(`${e.which}`);
 }
