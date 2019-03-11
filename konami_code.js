73f2fc5e986e886623013116ba63145afaac80d3
Justine B <justinegayton@gmail.com>
2019-03-11 20:42:01 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -3,6 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0 
 
 function init() {
+  function 
   document.body.addEventListener('keydown', function(e){
   const key = parseInt(e.detail || e.location);
   console.log(`${e.detail}`);
